4103adb9efeb5b947aa613f09b4dd8701cac6631
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-10-01 16:46:13 +0200

    commentaires de random

--- tribes/utils/random.py
+++ tribes/utils/random.py
@@ -1,10 +1,11 @@
+"""Ensemble de fonctions utilitaires pour le random"""
+
+# -------------------------------- IMPORTS ------------------------------------
 import random
-
 from tribes.utils import CappedFloat_0_1
-
 
 def gauss01(sigma:float=0.25)->CappedFloat_0_1:
     """Renvoie un nombre entre 0 et 1 selon une distribution 
-    gaussienne."""
+    gaussienne centrée en 0.5."""
     random_value = random.gauss(mu=0.5, sigma=sigma)
     return CappedFloat_0_1(random_value).value
