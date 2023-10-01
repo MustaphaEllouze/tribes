1afe76648ccd2edcaa596f9f7c6ddefa8ccd3495
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-10-01 15:46:15 +0200

    commentaires CappedNumber

--- tribes/utils/CappedNumbers.py
+++ tribes/utils/CappedNumbers.py
@@ -1,23 +1,53 @@
+"""Permet de générer des nombres avec bornes supérieres et inférieures."""
+
+# -------------------------------- IMPORTS ------------------------------------
 from dataclasses import dataclass
 from typing import Self
 
+# --------------------------------- CLASSE ------------------------------------
 class CappedNumber :
+    """Classe permettant de générer des sous-classes avec bornes inférieures 
+    et supérieures."""
 
-    def __init__(
-            self,
-    ):
-        raise ValueError("Cannot instantiate.")
+    def __new__(cls, *args, **kwargs) -> Self:
+        """This class cannot be instanciated"""
+        if cls is CappedNumber:
+            raise NotImplementedError("Cannot instanciate CappedNumber.")
+        return object.__new__(cls, *args, *kwargs)
     
     @classmethod
     def generate_type(
             self, 
-            number_type : float | int,
+            number_type : type        = float,
             min_val     : float | int = None,
             max_val     : float | int = None,
-    ):
+    )->type:
+        """Renvoie une classe dont le comportement est identique à float ou int
+        , qui inclut une gestion de bornes supérieure et inférieure. Chaque
+        opération fait en sorte de renvoyer une des deux bornes en cas 
+        d'overflow.
+
+        Args:
+            number_type (type, optional): Classe à laquelle doit ressembler 
+            le résultat (float ou int). Defaults to float.
+
+            min_val (float | int, optional): Borne inférieure. 
+            Defaults to None.
+
+            max_val (float | int, optional): Borne supérieure.
+            Defaults to None.
+
+        Returns:
+            type: Une classe se comportant comme number_type et implémentant 
+            automatiquement une gestion des bornes supérieure et inférieure.
+        """
         
         
         class CappedClass : 
+            """Classe de retour
+            """
+
+            # Attributs de classe : bornes supérieure et inférieure
             min_value   : float | int  = min_val
             max_value   : float | int  = max_val
 
@@ -25,14 +55,21 @@ class CappedNumber :
                     self, 
                     value : float | int,
             ):
-                self.value = value
+                """
+                Args:
+                    value (float | int): La valeur. Peut dépasser les bornes.
+                """
+                _value = value
 
+                # Si les bornes suéprieur et inférieure sont définies, on 
+                # replace la valeur dans le bon intervalle
                 if CappedClass.min_value is not None : 
-                    self.value = max(self.value, CappedClass.min_value)
+                    _value = max(_value, CappedClass.min_value)
                 if CappedClass.max_value is not None : 
-                    self.value = min(self.value, CappedClass.max_value)
+                    _value = min(_value, CappedClass.max_value)
                 
-                self.value      = number_type(self.value)
+                # Attribut
+                self.value      = number_type(_value)
             
             def __str__(self) -> str:
                 return f"{self.value}"
@@ -41,7 +78,17 @@ class CappedNumber :
             def cast_to_number(
                 cls,
                 value : number_type | Self,
-            ):
+            )->number_type:
+                """Permet de convertir des nombre et des CappedClass en nombre
+
+                Args:
+                    value (number_type | Self): La valeur à convertir
+
+                Returns:
+                    number_type: Valeur convertie
+                """
+
+                # On récupère la valeur s'il y a lieu
                 if isinstance(value, CappedClass):
                     true_value = value.value
                 elif isinstance(value, (float, int)):
@@ -52,6 +99,10 @@ class CappedNumber :
                 return number_type(true_value)
 
             def get_value(self, )->number_type:
+                """
+                Returns:
+                    number_type: La valeur stockée dans l'instance
+                """
                 return self.value
             
             def __add__(self, value : number_type | Self):
