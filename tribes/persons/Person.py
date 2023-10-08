734900831fb45006be235b3a338949d8958d6e9d
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-10-08 21:23:27 +0200

    prepare functions or behaviour

--- tribes/persons/Person.py
+++ tribes/persons/Person.py
@@ -3,6 +3,8 @@
 # -------------------------------- IMPORTS ------------------------------------
 
 from dataclasses import dataclass, field
+from enum import Enum
+from typing import Iterable
 
 from tribes.utils import (
     CappedFloat_0_1, 
@@ -46,11 +48,22 @@ def default_reputation()->CappedFloat_0_1:
     """Default factory pour reputation"""
     return CappedFloat_0_1(1.0)
 
+
+# ---------------------------- UTILS FUNCTIONS --------------------------------
+class HeightUnits(Enum):
+    M  = 'm'
+    CM = 'cm'
+    MM = 'mm'
+class MassUnits(Enum):
+    KG  = 'kg'
+    G  = 'g'
+
 # -------------------------------- CLASSE -------------------------------------
 @dataclass
 class Person :
     """Représente une personne. Structure de donnée centrale du module."""
 
+    # -------------------------------------------------------------------------
     # Nom de la personne 
     name                : str 
 
@@ -73,13 +86,13 @@ class Person :
     hierarchy           : HierarchyEnum
 
     # Liste des compétences connues par la personne
-    skills              : list[SkillEnum]
+    skills              : Iterable[SkillEnum]
 
     # Liste des travaux endossés par la personne 
-    job                 : list[JobEnum]
+    job                 : Iterable[JobEnum]
 
     # Liste des problèmes médicaux de la personne 
-    medical_condition   : list[MedicalConditionEnum]
+    medical_condition   : Iterable[MedicalConditionEnum]
 
     # Personnalité de la personne
     personality_traits  : Personality = Personality.random_personnality()
@@ -106,3 +119,173 @@ class Person :
     # Réputation de la personne 
     reputation          : CappedFloat_0_1   = field(
                                             default_factory=default_reputation)
+    
+    # -------------------------------------------------------------------------
+
+    def grow_older(
+            self, 
+            age : float | int,
+    )->None:
+        raise NotImplementedError()
+    
+    def grow(
+            self, 
+            height_rate : float | int, 
+            units : str = HeightUnits.M.value,
+    )->None :
+        raise NotImplementedError()
+    
+    def shrink(
+            self, 
+            height_rate : float | int, 
+            units : str = HeightUnits.M.value,
+    )->None :
+        raise NotImplementedError()
+    
+    def gain_weight(
+            self, 
+            mass_rate : float | int, 
+            units : str = HeightUnits.M.value,
+    )->None :
+        raise NotImplementedError()
+    
+    def lose_weight(
+            self, 
+            mass_rate : float | int, 
+            units : str = HeightUnits.M.value,
+    )->None :
+        raise NotImplementedError()
+    
+
+    def set_hierarchy(
+            self,
+            hierarchy_level,
+    )->None :
+        raise NotImplementedError()
+    
+    def raise_in_hierarchy(
+            self, 
+            number_of_levels,
+    )->None:
+        raise NotImplementedError()
+    
+    def fall_in_hierarchy(
+            self, 
+            number_of_levels,
+    )->None:
+        raise NotImplementedError()
+    
+    def add_skill(
+            self, 
+            a_skill : SkillEnum,
+    )->None:
+        raise NotImplementedError()
+    
+    def add_skills(
+            self, 
+            list_of_skills : Iterable[SkillEnum],
+    )->None:
+        raise NotImplementedError()
+    
+    def remove_skill(
+            self, 
+            a_skill : SkillEnum,
+    )->None:
+        raise NotImplementedError()
+    
+    def remove_skills(
+            self, 
+            list_of_skills : Iterable[SkillEnum],
+    )->None:
+        raise NotImplementedError()
+    
+
+    def add_job(
+            self, 
+            a_job : JobEnum,
+    )->None:
+        raise NotImplementedError()
+    
+    def add_jobs(
+            self, 
+            list_of_jobs : Iterable[JobEnum],
+    )->None:
+        raise NotImplementedError()
+    
+    def remove_job(
+            self, 
+            a_job : JobEnum,
+    )->None:
+        raise NotImplementedError()
+    
+    def remove_jobs(
+            self, 
+            list_of_jobs : Iterable[JobEnum],
+    )->None:
+        raise NotImplementedError()
+    
+    def add_condition(
+            self, 
+            a_condition : MedicalConditionEnum,
+    )->None:
+        raise NotImplementedError()
+    
+    def add_conditions(
+            self, 
+            list_of_conditions : Iterable[MedicalConditionEnum],
+    )->None:
+        raise NotImplementedError()
+    
+    def remove_condition(
+            self, 
+            a_condition : MedicalConditionEnum,
+    )->None:
+        raise NotImplementedError()
+    
+    def remove_conditions(
+            self, 
+            list_of_conditions : Iterable[MedicalConditionEnum],
+    )->None:
+        raise NotImplementedError()
+    
+    def change_personnality(
+            self, 
+            personnality : Personality,
+    )->None:
+        raise NotImplementedError()
+
+    def kill(
+            self,
+    )->None:
+        raise NotImplementedError()
+    
+    def change_hunger(
+            self, 
+            rate : float | int,
+    )->None :
+        raise NotImplementedError()
+    
+    def change_thirst(
+            self, 
+            rate : float | int,
+    )->None :
+        raise NotImplementedError()
+    
+    def change_happiness(
+            self, 
+            rate : float | int,
+    )->None :
+        raise NotImplementedError()
+    
+    def change_fertility(
+            self, 
+            rate : float | int,
+    )->None :
+        raise NotImplementedError()
+    
+    def change_reputation(
+            self, 
+            rate : float | int,
+    )->None :
+        raise NotImplementedError()
+    
